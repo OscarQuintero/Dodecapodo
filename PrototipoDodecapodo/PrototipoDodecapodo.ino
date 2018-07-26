e45e43cfb3cb14f9b3b33223c43feb7ac84ccfda
Oscar Quintero <oscar24quintero@hotmail.com>
2018-07-26 16:51:16 -0500

    Initial commit

---
+++ PrototipoDodecapodo/PrototipoDodecapodo.ino
@@ -0,0 +1,64 @@
+//Version 1.0 del firmware para el Dodecapodo PACO
+//------------------------------------------------
+//Entradas del puente en H
+int PHIN1 = 22;
+int PHIN2 = 23;
+int PHIN3 = 26;
+int PHIN4 = 25;
+
+int MotoresDerechos = 2;
+int MotoresIzquierdos = 3;
+
+//-----Declaracion de funciones
+void detente()
+{
+  
+}
+
+void avanza(int velocidad)
+{
+  digitalWrite(PHIN1, HIGH);
+  digitalWrite(PHIN2, LOW);
+  digitalWrite(PHIN3, LOW);
+  digitalWrite(PHIN4, HIGH);
+  analogWrite(MotoresDerechos, velocidad);
+  analogWrite(MotoresIzquierdos, velocidad);
+  
+}
+
+void retrocede(int velocidad)
+{
+  
+}
+
+void giraDerecha(int velocidad)
+{
+  
+}
+
+void giraIzquierda(int velocidad)
+{
+  
+}
+
+
+
+//------------------------------------------------
+void setup() {
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+
+  avanza(255);
+  delay(1000);
+  detente();
+
+  digitalWrite(13, LOW);
+  delay(200);
+
+  
+
+}
