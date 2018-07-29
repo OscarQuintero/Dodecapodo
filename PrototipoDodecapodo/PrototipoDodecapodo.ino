f4c71082b58496c6c82d8b459c990e928d85fe4a
Oscar Quintero <oscar24quintero@hotmail.com>
2018-07-28 22:50:00 -0500

    Correccion de la direccion del Puente H

--- PrototipoDodecapodo/PrototipoDodecapodo.ino
+++ PrototipoDodecapodo/PrototipoDodecapodo.ino
@@ -4,10 +4,11 @@
 
 //#include "sensores.h"
 
-int PHIN1 = 22;
-int PHIN2 = 23;
-int PHIN3 = 26;
-int PHIN4 = 25;
+int PHIN1 = 22;//Corresponde al lado derecho adelante
+int PHIN2 = 23;//Corresponde al lado izquierdo atras
+int PHIN3 = 26;//Corresponde al lado izquierdo adelante
+int PHIN4 = 25;//Corresponde al lado derecho atras
+ 
 
 int MotoresDerechos = 2;
 int MotoresIzquierdos = 3;
@@ -48,19 +49,19 @@ void retrocede(int velocidad)
 void giraDerecha(int velocidad)
 {
   digitalWrite(PHIN1,LOW);
-  digitalWrite(PHIN2,HIGH);
+  digitalWrite(PHIN2,LOW);
   digitalWrite(PHIN3,HIGH);
-  digitalWrite(PHIN4,LOW);
+  digitalWrite(PHIN4,HIGH);
   analogWrite(MotoresDerechos, velocidad);
   analogWrite(MotoresIzquierdos, velocidad);
 }
 
 void giraIzquierda(int velocidad)
 {
-  digitalWrite(PHIN1,HIGH);
-  digitalWrite(PHIN2,LOW);
-  digitalWrite(PHIN3,LOW);
-  digitalWrite(PHIN4,HIGH);
+  digitalWrite(PHIN1,HIGH);  //Corresponde al lado derecho adelante
+  digitalWrite(PHIN2,HIGH);  //Corresponde al lado izquierdo atras
+  digitalWrite(PHIN3,LOW); //Corresponde al lado izquierdo adelante
+  digitalWrite(PHIN4,LOW);  //Corresponde al lado derecho atras
   analogWrite(MotoresDerechos, velocidad);
   analogWrite(MotoresIzquierdos, velocidad);
 }
@@ -92,15 +93,32 @@ void setup() {
   pinMode(MotoresDerechos, OUTPUT);
   pinMode(MotoresIzquierdos, OUTPUT);
 
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
+  
 
   avanza(255);
+  delay(2000);
+  detente();
+  delay(2000);
+  
+  retrocede(255);
   delay(1000);
   detente();
+  delay(1000);
 
+  digitalWrite(13, HIGH);
+  
+  giraIzquierda(100);
+  delay(6000);
+  detente();
+
+  
+  
   digitalWrite(13, LOW);
   delay(200);
 
